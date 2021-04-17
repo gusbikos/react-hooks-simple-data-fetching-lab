6bff166c5756d6a82a741af57dcf844919a21629
gusbikos <gusbikos@gmail.com>
2021-04-17 17:38:12 -0400

    commit tree 8e07b85682fd7371e91a96efb027a07ea946f0e1

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import React, { useEffect, useState } from "react"
+
+function App() {
+    const [dog, setDog] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(dog => {
+            setDog(dog.message)
+            console.log(dog)
+        })
+    }, []) // put dog, and dog.message in dependancies array to see different actions
+
+    if (!dog) return ( <p>Loading...</p>)
+
+    console.log("render first")
+
+    return (<img src={dog} alt="A Random Dog"/>)
+
+}
+
+export default App
